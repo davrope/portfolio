0dcb9e847ba1f5f3426489d1e9b3f03e347ba465
David <rodpe.david@gmail.com>
2022-01-06 23:11:51 -0600

    Projects page

---
+++ client/src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+
+            <h2>Hi, there!</h2>
+            <h4>Welcome to my Web Developer Portfolio.</h4>
+            <p>
+                My name is David Rodríguez and I am passionate about technology and building solutions for people and companies.
+            </p>
+
+
+        </div>
+    )
+}
+
+export default About
---
+++ client/src/components/Projects.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import tshirt from '../images/tshirt.jpg';
+import productivityimg from '../images/productivity.jpg'
+
+
+const Projects = () => {
+    return (
+        <div>
+            <h2>
+                Here you can see some of my web developer projects:
+            </h2>
+
+            <div className='row'>
+
+                <div className='column'>
+                    <div className='card'>
+                    <img src={tshirt} alt="Jane" style={{width:100}}/>
+                    <div className='container'>
+                        <h2>My Size</h2>
+                        <p className='description'>
+                            Front end created with react and the pose-detection API from Tensorflow. It helps you to decide which size of clothes would be the better fit considering the store database
+                        </p>
+                        <p><button className='button'>Live Demo</button> </p>
+                    </div>
+                    
+                    </div>
+                </div>
+
+                <div className='column'>
+                    <div className='card'>
+                    <img src={productivityimg} alt="Jane" style={{width:100}}/>
+                    <div className='container'>
+                        <h2>Productivity app</h2>
+                        <p className='description'>
+                            Track the time of your projects with a simple but effective productivity app that includes a to-do list
+                        </p>
+                        <p><button className='button'>Live Demo</button> </p>
+                    </div>
+                     
+                    </div>
+                </div>
+
+            </div>
+
+
+            
+
+            
+
+        </div>
+    )
+}
+
+export default Projects
