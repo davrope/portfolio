f459ab5f228debf24d50b2e39798829d46d9aa70
David <rodpe.david@gmail.com>
2022-01-17 22:47:47 -0600

    Contact changes for functionality

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react';
+
 
 const Contact = () => {
+   const [status, setStatus] = useState("Submit")
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        setStatus("Sending...");
+        const {name, email, message} = e.target.elements;
+
+        let details = {
+            name: name.value,
+            email: email.value,
+            message: message.value
+        };
+
+        let response = await fetch("http://localhost:5000/contact", {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(details),
+        });
+
+        setStatus("Submit");
+        let result = await response.json();
+        alert(result.status);
+    }
+
     return (
         <div id = 'contact' style={{background: 'black', height:'100vh', width:'100vw'}}>
             <div  style= {{margin: 0, position:'relative', top:'35%'}}>
@@ -11,32 +38,32 @@ const Contact = () => {
                     <h4>Let's work together</h4>
                 </div>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className='d-flex justify-content-center'>
                         <div className='form-group'>
-                            <label for = "Name" style={{color:'grey'}}>Name</label>
-                            <input type = 'string' className='form-control' id = 'name' aria-describedby="namehelp" placeholder="What's your name?"/>
+                            <label htmlFor = "name" style={{color:'grey'}}>Name: </label>
+                            <input type = 'text' className='form-control' id = 'name' aria-describedby="namehelp" placeholder="What's your name?" required/>
                         </div>
                     </div>
 
                     <div className='d-flex justify-content-center'>
                         <div className='form-group'>
-                            <label for = "inputEmail" style={{color:'grey'}}>Email address</label>
-                            <input type = 'email' className='form-control' id = 'inputEmail' aria-describedby="emailHelp" placeholder="Enter email"/>
+                            <label htmlFor = "email" style={{color:'grey'}}>Email address</label>
+                            <input type = 'email' className='form-control' id = 'email' aria-describedby="emailHelp" placeholder="Enter email" required/>
                             {/* <small id = "emailhelp" className=' form-text text-muted'>I'll never share your email</small> */}
                         </div>
                     </div>
 
                     <div className='d-flex justify-content-center'>
                         <div className='form-group'>
-                            <label for = "comment" style={{color:'grey'}}>Describe your idea</label>
-                            <input type = 'string' className='form-control' id = 'describeidea' aria-describedby="emailHelp" placeholder="Enter email"/>
+                            <label htmlFor = "message" style={{color:'grey'}}>Describe your idea</label>
+                            <input type = 'string' className='form-control' id = 'message' aria-describedby="message" placeholder="Your idea" required/>
                             
                         </div>
                     </div>
 
                     <div className='d-flex justify-content-center'>
-                        <button type='submit' className='btn btn-primary'>Submit</button>
+                        <button type='submit' className='btn btn-primary'>{status} </button>
                     </div>
 
                 </form>
--- index.js
+++ index.js
@@ -1,49 +1,60 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const bodyParser = require('body-parser');
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer")
 
-const keys = require('./config/keys');
-require('./models/User');
-require('./models/Project');
-require('./services/passport');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI,  { useNewUrlParser: true, useUnifiedTopology: true});
+
+// const mongoose = require('mongoose');
+// const cookieSession = require('cookie-session');
+// const passport = require('passport');
+// const bodyParser = require('body-parser');
+
+// const keys = require('./config/keys');
+// require('./models/User');
+// require('./models/Project');
+// require('./services/passport');
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(keys.mongoURI,  { useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(
-    cookieSession({
-        maxAge: 30*24*60*60*1000,
-        keys: [keys.cookieKey]                                                                                                                                                                                                                                                      
-    })
-);
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
 
-app.use(passport.initialize());
-app.use(passport.session());
 
-require('./routes/authRoutes')(app);
-// require('./routes/billingRoutes')(app);
-require('./routes/projectRoutes')(app);
+// app.use(bodyParser.json());
+// app.use(
+//     cookieSession({
+//         maxAge: 30*24*60*60*1000,
+//         keys: [keys.cookieKey]                                                                                                                                                                                                                                                      
+//     })
+// );
 
-if (process.env.NODE_ENV === 'production'){
-    //Express will serve up production assets
-    //like our main.js file, or main.css file
-    app.use(express.static('client/build'));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-    //Express will serve up the index.html file
-    //if it doesn't recognize the route
-    const path = require('path');
-    app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
-}
+// require('./routes/authRoutes')(app);
+// // require('./routes/billingRoutes')(app);
+// require('./routes/projectRoutes')(app);
+
+// if (process.env.NODE_ENV === 'production'){
+//     //Express will serve up production assets
+//     //like our main.js file, or main.css file
+//     app.use(express.static('client/build'));
+
+//     //Express will serve up the index.html file
+//     //if it doesn't recognize the route
+//     const path = require('path');
+//     app.get('*', (req, res)=>{
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     });
+// }
 
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+app.listen(PORT, ()=> console.log("Server Running"));
